39a07954e6886b253cbd3752b1502741556a3f54
Mark Merritt <markmerritt804@gmail.com>
2019-05-17 15:45:34 -0400

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when "notice" then "alert-dark"
+      when "success" then "alert-success"
+      when "error" then "alert-danger"
+      when "alert" then "alert-warning"
+    end
+  end
 end
