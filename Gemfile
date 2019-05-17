4e730b694d405e79590d761b4e8924585d1e2612
Mark Merritt <markmerritt804@gmail.com>
2019-05-17 14:34:56 -0400

    Setup test suite, Add welcome controller

--- Gemfile
+++ Gemfile
@@ -14,6 +14,13 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 3.8', '>= 3.8.2'
+  gem 'factory_bot_rails', '~> 5.0', '>= 5.0.2'
+end
+
+group :test do 
+  gem 'capybara', '~> 3.20'
+  gem 'database_cleaner', '~> 1.7'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -13,23 +13,21 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module PatriotSwap
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.system_tests = nil
+      g.test_framework :rspec
+      g.helper_specs false
+      g.controller_specs false
+      g.view_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
   end
 end
