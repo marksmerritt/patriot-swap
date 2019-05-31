c1daa10630ad2be731fe7a5ce6ee1c6d81eec30d
Mark Merritt <markmerritt804@gmail.com>
2019-05-30 21:13:19 -0400

    Remove acts_as_taggable_on (too many open issues with Rails 6)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_224909) do
+ActiveRecord::Schema.define(version: 2019_05_31_011020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,33 +38,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_224909) do
     t.integer "price_cents", default: 0, null: false
     t.bigint "book_id", null: false
     t.index ["book_id"], name: "index_listings_on_book_id"
-  end
-
-  create_table "taggings", id: :serial, force: :cascade do |t|
-    t.integer "tag_id"
-    t.string "taggable_type"
-    t.integer "taggable_id"
-    t.string "tagger_type"
-    t.integer "tagger_id"
-    t.string "context", limit: 128
-    t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
-  end
-
-  create_table "tags", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "terms", force: :cascade do |t|
@@ -105,5 +78,4 @@ ActiveRecord::Schema.define(version: 2019_05_30_224909) do
   end
 
   add_foreign_key "listings", "books"
-  add_foreign_key "taggings", "tags"
 end
