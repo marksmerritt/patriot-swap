b0acb8159327ae7c3b7b3d4015718748b9d504bc
Mark Merritt <markmerritt804@gmail.com>
2019-05-30 22:56:50 -0400

    Add books show action

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -13,14 +13,37 @@ describe "navigation" do
     end
   end
 
-  describe "new" do 
-    it "can be created through a form" do 
-      visit new_book_path
-      fill_in "title-field", with: "Some Title"
-      fill_in "isbn-field", with: "Some ISBN"
-      fill_in "tag-names-field", with: "IT400"
+  describe "show" do 
+    before do 
+      @book = FactoryBot.create(:book)
+    end
 
-      expect{ click_button "submit-btn" }.to change{ Book.count }.by(1)
+    it "can be reached from the index page" do 
+      visit books_path
+      click_link "book-#{@book.id}-show"
+
+      expect(current_path).to eq(book_path(@book))
+    end
+  end
+
+  describe "new" do 
+    context "with valid input" do 
+      it "can be created through a form" do 
+        visit new_book_path
+        fill_in "title-field", with: "Some Title"
+        fill_in "isbn-field", with: "Some ISBN"
+        fill_in "tag-names-field", with: "IT400"
+
+        expect{ click_button "submit-btn" }.to change{ Book.count }.by(1)
+      end
+    end
+
+    context "with invalid input" do 
+      it "cannot be created through the form" do 
+        visit new_book_path
+
+        expect{ click_button "submit-btn" }.to change{ Book.count }.by(0)
+      end
     end
   end
 end
