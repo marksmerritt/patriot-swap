31c5a1f9b2494ba33aa27d214140e8c9666684c3
Mark Merritt <markmerritt804@gmail.com>
2019-05-30 22:44:31 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
-# Reset
-User.delete_all
+require 'faker'
 
+# Reset
+Listing.delete_all
+Book.delete_all
+User.delete_all
 
 
 # Create Users
@@ -17,3 +20,47 @@ admin_user.skip_confirmation!
 admin_user.save!
 
 puts "Admin User created"
+
+10.times do 
+  u = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "helloworld", 
+    password_confirmation: "helloworld"
+  )
+
+  u.skip_confirmation!
+  u.save!
+end
+
+@users = User.all
+puts "#{User.count} users created"
+
+# Create Books
+
+20.times do 
+  Book.create!(
+    title: Faker::Book.title,
+    isbn: Faker::Code.isbn
+  )
+end
+
+@books = Book.all
+puts "#{Book.count} books created"
+
+
+# Create Listings
+
+100.times do 
+  Listing.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    seller: @users.sample,
+    price_cents: Faker::Number.number(4),
+    book: @books.sample
+  )
+end
+
+@listings = Listing.all
+puts "#{Listing.count} listings created"
