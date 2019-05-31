80b7d75ea9de251581bfa6c4c129076b61068efa
Mark Merritt <markmerritt804@gmail.com>
2019-05-30 22:16:05 -0400

    Add books index action

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :book do
     title { "The Pragmatic Programmer: From Journeyman to Master" }
     isbn { "978-0201616224" }
+    tags_as_string { "IT700" }
+  end
+
+  factory :second_book, class: Book do
+    title { "The Second Book" }
+    isbn { "978-0201611234" }
+    tags_as_string { "IT800" }
   end
 end
--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper"
 
 describe "navigation" do 
+  describe "index" do 
+    before do 
+      @book1 = FactoryBot.create(:book)
+      @book2 = FactoryBot.create(:second_book)
+    end
+
+    it "displays a list of books" do 
+      visit books_path
+      expect(page).to have_content(@book1.title).and have_content(@book2.title)
+    end
+  end
+
   describe "new" do 
     it "can be created through a form" do 
       visit new_book_path
