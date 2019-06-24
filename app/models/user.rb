3eae41917b1253c1796486256097006abf454fe0
Mark Merritt <markmerritt804@gmail.com>
2019-06-24 18:41:44 -0400

    Revert "Add models for chat"
    This reverts commit 6f1e08170267149bb6c1745e38eaaa4d95b36bb0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,6 @@ class User < ApplicationRecord
   delegate :longitude, :latitude, to: :location
 
   has_many :listings, foreign_key: "seller_id"
-  has_many :chatroom_users
-  has_many :chatrooms, through: :chatroom_users 
-  has_many :messages
-
   belongs_to :location
 
   accepts_nested_attributes_for :location
