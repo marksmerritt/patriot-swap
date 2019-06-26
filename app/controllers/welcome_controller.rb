0d66383c86bc32ec551eb78b8cc88de07ad94510
Mark Merritt <markmerritt804@gmail.com>
2019-06-26 14:59:20 -0400

    Add listing search to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to buyers_path(q: params[:q]) if params[:q]
   end
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,9 +2,19 @@ require "rails_helper"
 
 describe "navigation" do 
   describe "index" do 
-    it "can be reached successfully" do 
+    before do 
       visit root_path
-      expect(page.status_code).to eq(200)
+    end
+
+    it "has a search form for listings" do 
+      fill_in "listing-search-field", with: "IT100"
+      click_button "listing-search-submit"
+      expect(current_path).to eq(buyers_path)
+    end
+
+    it "has a link to browse listings" do 
+      click_link "browse-listings-link"
+      expect(current_path).to eq(buyers_path)
     end
   end
 end
