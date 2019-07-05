08b2b09e593e00fb32408aac464225fa8e2c63f3
Mark Merritt <markmerritt804@gmail.com>
2019-07-04 23:50:25 -0400

    Just a better way...

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -3,8 +3,8 @@ import Typed from "typed.js"
 document.addEventListener("turbolinks:load", () => {
   var typed = new Typed("#typed", {
     startDelay: 1000,
-    strings: ["Buy Textbooks.", "Sell Textbooks.", "Trade Textbooks."],
-    backSpeed: 40,
+    strings: ["Buy", "Sell", "Trade"],
+    backSpeed: 80,
     typeSpeed: 80,
     loop: true
   });
