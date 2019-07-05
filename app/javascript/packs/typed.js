c1b270d1fe85913f01000db339fa5e638447b185
Mark Merritt <markmerritt804@gmail.com>
2019-07-04 23:26:26 -0400

    More emphasis

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -3,7 +3,7 @@ import Typed from "typed.js"
 document.addEventListener("turbolinks:load", () => {
   var typed = new Typed("#typed", {
     startDelay: 1000,
-    strings: ["Buy Textbooks", "Sell Textbooks", "Trade Textbooks"],
+    strings: ["Buy Textbooks.", "Sell Textbooks.", "Trade Textbooks."],
     backSpeed: 40,
     typeSpeed: 80,
     loop: true
