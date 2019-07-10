498cc0fa7b1b3cb55ed704e11922a03b902abf8c
Mark Merritt <markmerritt804@gmail.com>
2019-07-10 19:47:01 -0400

    Change convo title on first message submit

--- app/javascript/channels/conversation_channel.js
+++ app/javascript/channels/conversation_channel.js
@@ -10,6 +10,9 @@ consumer.subscriptions.create("ConversationChannel", {
   },
 
   received(data) { 
+    if ( $(".chat_message").length === 0 ) {
+      $(".chatbox__title").text("Message sent!");
+    }
     $(`#conversation-btw-${data.seller_id}-${data.buyer_id}`).append(`<p class="chat__message chat__message--live">${data.sender_name}: ${data.body}</p>`);
     $(`#new-message-body-${data.sender_id}`).val("");
   }
